2f04aed8139b77ed9545997a68f555da34c88ce7
👻 Albert 👻 <wentaoli@microsoft.com>
2026-01-13 03:12:56 +0800

    add template codes for the next experiment try02

--- src/add_cuda.cu
+++ src/add_cuda.cu
@@ -1,3 +1,4 @@
+#include <type_traits>
 #include "cuda_fp16.h"
 
 template <typename T>
@@ -7,29 +8,26 @@ __device__ T add(const T& a, const T& b)
     {
         return __hadd(a, b);
     }
-
-    if constexpr (std::is_same_v<T, half2>)
+    else if constexpr (std::is_same_v<T, half2>)
     {
         return __hadd2(a, b);
     }
-
-    if constexpr (std::is_same_v<T, float2>)
+    else if constexpr (std::is_same_v<T, float2>)
     {
         return make_float2(a.x + b.x, a.y + b.y);
     }
-
-    if constexpr (std::is_same_v<T, float3>)
+    else if constexpr (std::is_same_v<T, float3>)
     {
         return make_float3(a.x + b.x, a.y + b.y, a.z + b.z);
     }
-
-    if constexpr (std::is_same_v<T, float4>)
+    else if constexpr (std::is_same_v<T, float4>)
     {
         return make_float4(a.x + b.x, a.y + b.y, a.z + b.z, a.w + b.w);
     }
-
-
-    return a + b;
+    else
+    {
+        return a + b;
+    }
 }
 
 template <typename T>
@@ -43,8 +41,17 @@ __global__ void vector_add_kernel(T* c, const T* a, const T* b, size_t n, size_t
 }
 
 // C++ callable wrapper function
-void vector_add(float* c, const float* a, const float* b, int n, dim3 grid_dim, dim3 block_dim)
+template <typename T>
+void vector_add(T* c, const T* a, const T* b, size_t n, dim3 grid_dim, dim3 block_dim)
 {
     size_t step = block_dim.x * grid_dim.x;
-    vector_add_kernel<float><<<grid_dim, block_dim>>>(c, a, b, n, step);
+    vector_add_kernel<T><<<grid_dim, block_dim>>>(c, a, b, n, step);
 }
+
+template void vector_add<float>(float* c, const float* a, const float* b, size_t n, dim3 grid_dim, dim3 block_dim);
+template void vector_add<float2>(float2* c, const float2* a, const float2* b, size_t n, dim3 grid_dim, dim3 block_dim);
+template void vector_add<float3>(float3* c, const float3* a, const float3* b, size_t n, dim3 grid_dim, dim3 block_dim);
+template void vector_add<float4>(float4* c, const float4* a, const float4* b, size_t n, dim3 grid_dim, dim3 block_dim);
+
+template void vector_add<half>(half* c, const half* a, const half* b, size_t n, dim3 grid_dim, dim3 block_dim);
+template void vector_add<half2>(half2* c, const half2* a, const half2* b, size_t n, dim3 grid_dim, dim3 block_dim);
--- src/add_cuda.h
+++ src/add_cuda.h
@@ -3,4 +3,5 @@
 #include <cuda_runtime.h>
 
 // C++ callable wrapper function
-void vector_add(float *c, const float *a, const float *b, int n, dim3 grid_dim, dim3 block_dim);
+template <typename T>
+void vector_add(T* c, const T* a, const T* b, size_t n, dim3 grid_dim, dim3 block_dim);
