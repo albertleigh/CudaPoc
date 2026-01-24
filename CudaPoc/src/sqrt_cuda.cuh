bb303c06da7df1ffb5088791aacb9ee4a153e6c4
👻 Albert 👻 <liwentao90@yahoo.com>
2026-01-25 02:05:03 +0800

    add_one sqrt kernel

---
+++ CudaPoc/src/sqrt_cuda.cuh
@@ -0,0 +1,17 @@
+//
+// Created by ali on 1/25/26.
+//
+
+#pragma once
+
+#include <cuda_runtime.h>
+namespace cuda_poc {
+// CUDA kernel (callable from .cu files only)
+template <typename T>
+__global__ void sqrt_kernel(T* c, const T* a, size_t n, size_t step);
+
+// C++ callable wrapper function
+template <typename T>
+void vector_sqrt(T* c, const T* a, size_t n, dim3 grid_dim, dim3 block_dim);
+
+}  // namespace cuda_poc
