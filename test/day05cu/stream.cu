b5fc440ce66f59056d1acf32dd0d01ec5fdf72ff
👻 Albert 👻 <liwentao90@yahoo.com>
2026-01-25 00:22:34 +0800

    stream example

--- test/day05cu/stream.cu
+++ test/day05cu/stream.cu
@@ -59,6 +59,16 @@ class CudaPoc_Day0502_Stream : public ::testing::Test {
      ...);
   }
 
+  template <typename... Args>
+  void free_async_device_ptr(cudaStream_t stream, Args*... ptrs) {
+    (([&]() {
+       if (ptrs) {
+         CUDA_CHECK(cudaFreeAsync(ptrs, stream));
+       }
+     })(),
+     ...);
+  }
+
   void test_streams_with_kernel_fun(
       std::function<
           void(dim3& grid, dim3& block, float* d_a, float* d_b, float* d_c, float* d_d, size_t SIZE, size_t step)>
@@ -115,6 +125,9 @@ TEST_F(CudaPoc_Day0502_Stream, Stream01) {
 
         vector_add_kernel<<<grid_dim, block_dim, 0, stream1>>>(d_c, d_a, d_b, SIZE, step);
         vector_mul_kernel<<<grid_dim, block_dim, 0, stream2>>>(d_d, d_a, d_b, SIZE, step);
+        // Wait for all async operations in stream1 to complete before asserting
+        CUDA_CHECK(cudaStreamSynchronize(stream1));
+        CUDA_CHECK(cudaStreamSynchronize(stream2));
       });
 }
 
@@ -127,13 +140,15 @@ TEST_F(CudaPoc_Day0502_Stream, Stream02) {
 
         vector_add_kernel<<<grid_dim, block_dim>>>(d_c, d_a, d_b, SIZE, step);
         vector_mul_kernel<<<grid_dim, block_dim, 0, stream2>>>(d_d, d_a, d_b, SIZE, step);
+        // Wait for all async operations in stream1 to complete before asserting
+        CUDA_CHECK(cudaStreamSynchronize(stream2));
       });
 }
 
 TEST_F(CudaPoc_Day0502_Stream, Stream03) {
   test_streams_with_kernel_fun(
       [](dim3& grid_dim, dim3& block_dim, float* d_a, float* d_b, float* d_c, float* d_d, size_t SIZE, size_t step) {
-        cudaStream_t stream2;
+        // cudaStream_t stream2;
         // CUDA_CHECK(cudaStreamCreate(&stream1));
         // CUDA_CHECK(cudaStreamCreate(&stream2));
 
@@ -152,7 +167,110 @@ TEST_F(CudaPoc_Day0502_Stream, Stream04) {
 
         vector_add_kernel<<<grid_dim, block_dim>>>(d_c, d_a, d_b, SIZE, step);
         vector_mul_kernel<<<grid_dim, block_dim, 0, stream2>>>(d_d, d_a, d_b, SIZE, step);
+        // Wait for all async operations in stream1 to complete before asserting
+        CUDA_CHECK(cudaStreamSynchronize(stream2));
       });
+}
+
+// === Kernel: vector_add_wo_stream ===
+// Execution time: 3.48784 ms
+// Grid dimensions: (4096, 1, 1)
+// Block dimensions: (256, 1, 1)
+// Total threads: 1048576
+// GPU memory used: 0 MB
+// GPU memory free: 3260.25 MB / 4095.56 MB
+// Size used: 1048576
+//
+// === Kernel: vector_add_in_stream ===
+// Execution time: 3.14851 ms
+// Grid dimensions: (4096, 1, 1)
+// Block dimensions: (256, 1, 1)
+// Total threads: 1048576
+// GPU memory used: 0 MB
+// GPU memory free: 3240.25 MB / 4095.56 MB
+// Size used: 1048576
+
+TEST_F(CudaPoc_Day0502_Stream, AllocateInStream01) {
+  constexpr size_t SIZE = 1 << 20;  // 4MB for float
+  size_t size_bytes = SIZE * sizeof(float);
+
+  dim3 block_dim(256);
+  dim3 grid_dim((SIZE + block_dim.x - 1) / block_dim.x);
+
+  // float
+  {
+    std::vector<float> h_a(SIZE, 1);
+    std::vector<float> h_b(SIZE, 2);
+    std::vector<float> h_c(SIZE, 0);
+
+    float *d_a, *d_b, *d_c;
+    CUDA_CHECK(cudaMalloc(&d_a, size_bytes));
+    CUDA_CHECK(cudaMalloc(&d_b, size_bytes));
+    CUDA_CHECK(cudaMalloc(&d_c, size_bytes));
+
+    KernelConfig config(grid_dim, block_dim);
+    timeKernel(
+        "vector_add_wo_stream",
+        [&]() {
+          CUDA_CHECK(cudaMemcpy(d_a, h_a.data(), size_bytes, cudaMemcpyHostToDevice));
+          CUDA_CHECK(cudaMemcpy(d_b, h_b.data(), size_bytes, cudaMemcpyHostToDevice));
+          CUDA_CHECK(cudaMemcpy(d_c, h_c.data(), size_bytes, cudaMemcpyHostToDevice));
+          vector_add(d_c, d_a, d_b, SIZE, grid_dim, block_dim);
+          CUDA_CHECK(cudaGetLastError());
+          CUDA_CHECK(cudaDeviceSynchronize());
+          CUDA_CHECK(cudaMemcpy(h_c.data(), d_c, size_bytes, cudaMemcpyDeviceToHost));
+        },
+        &config);
+    std::cout << "Size used: " << SIZE << '\n';
+
+    assert_vector_equal(h_c, SIZE, 3.0f);
+    free_device_ptr(d_a, d_b, d_c);
+  }
+}
+
+TEST_F(CudaPoc_Day0502_Stream, AllocateInStream02) {
+  constexpr size_t SIZE = 1 << 20;  // 4MB
+  size_t size_bytes = SIZE * sizeof(float);
+
+  dim3 block_dim(256);
+  dim3 grid_dim((SIZE + block_dim.x - 1) / block_dim.x);
+
+  // float
+  {
+    std::vector<float> h_a(SIZE, 1);
+    std::vector<float> h_b(SIZE, 2);
+    std::vector<float> h_c(SIZE, 0);
+
+    cudaStream_t stream1;
+    // CUDA_CHECK(cudaStreamCreate(&stream1));
+    // Operations in this stream will not implicitly synchronize with the null stream
+    CUDA_CHECK(cudaStreamCreateWithFlags(&stream1, cudaStreamNonBlocking));
+
+    float *d_a, *d_b, *d_c;
+    CUDA_CHECK(cudaMallocAsync(&d_a, size_bytes, stream1));
+    CUDA_CHECK(cudaMallocAsync(&d_b, size_bytes, stream1));
+    CUDA_CHECK(cudaMallocAsync(&d_c, size_bytes, stream1));
+
+    KernelConfig config(grid_dim, block_dim);
+    timeKernel(
+        "vector_add_in_stream",
+        [&]() {
+          size_t step = block_dim.x * grid_dim.x;
+          CUDA_CHECK(cudaMemcpyAsync(d_a, h_a.data(), size_bytes, cudaMemcpyHostToDevice, stream1));
+          CUDA_CHECK(cudaMemcpyAsync(d_b, h_b.data(), size_bytes, cudaMemcpyHostToDevice, stream1));
+          CUDA_CHECK(cudaMemcpyAsync(d_c, h_c.data(), size_bytes, cudaMemcpyHostToDevice, stream1));
+          vector_add_kernel<<<grid_dim, block_dim, 0, stream1>>>(d_c, d_a, d_b, SIZE, step);
+          CUDA_CHECK(cudaGetLastError());
+          CUDA_CHECK(cudaMemcpyAsync(h_c.data(), d_c, size_bytes, cudaMemcpyDeviceToHost, stream1));
+          // Wait for all async operations in stream1 to complete before asserting
+          CUDA_CHECK(cudaStreamSynchronize(stream1));
+        },
+        &config);
+    std::cout << "Size used: " << SIZE << '\n';
+
+    assert_vector_equal(h_c, SIZE, 3.0f);
+    free_async_device_ptr(stream1, d_a, d_b, d_c);
+  }
 }
 
 }  // namespace cuda_poc::day05cu
